8ceb9d350aa01cbe9dc0eba117023d16bca8a9fc
josephpacificoIV <joejpacifico@gmail.com>
2022-08-01 11:25:38 -0400

    added global exception handling

--- field-agent/src/main/java/learn/field_agent/controllers/GlobalExceptionHandler.java
+++ field-agent/src/main/java/learn/field_agent/controllers/GlobalExceptionHandler.java
@@ -1,18 +1,43 @@
 package learn.field_agent.controllers;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-// @ControllerAdvice // 1
+@ControllerAdvice // 1
 public class GlobalExceptionHandler {
 
 
-    @ExceptionHandler(Exception.class) // 2
-    public ResponseEntity<String> handleException(Exception ex) { // 3
-        return new ResponseEntity<String>(
-                "Something went wrong on our end. Your request failed. :(",
+    @ExceptionHandler(DataAccessException.class) // 2
+    public ResponseEntity<ErrorResponse> handleException(DataAccessException ex) { // 3
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse("We can't show you the details, but something went wrong in our database. Sorry :("),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    // IllegalArgumentException is the super class for many Java exceptions
+    // including all formatting (number, date) exceptions.
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleException(IllegalArgumentException ex) {
+
+        // Log the exception?
+
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse(ex.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // This is our absolute last resort. Yuck.
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+
+        // Log the exception?
+
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse("Something went wrong on our end. Your request failed. :("),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
